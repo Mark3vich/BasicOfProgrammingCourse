ac84ab4d2dbd40e28e0bfe6eccccdc0e83331f02
Mark3vich <mango.2004@yandex.ru>
2023-04-15 00:03:27 +0300

    append access functions

--- vector.c
+++ vector.c
@@ -7,19 +7,15 @@ void outputVector(vector v) {
 }
 
 void test_pushBack_emptyVector(vector *v) { 
-    pushBack(v, 1);
-    pushBack(v, 2);
-    pushBack(v, 3);
-    pushBack(v, 4);
-    pushBack(v, 5);
+    for(size_t i = 1; i < 6; i++) { 
+        pushBack(v, i);
+    }
 }   
 
 void test_pushBack_fullVector(vector *v) { 
-    pushBack(v, 6);
-    pushBack(v, 7);
-    pushBack(v, 8);
-    pushBack(v, 9);
-    pushBack(v, 10);
+    for(size_t i = 6; i < 11; i++) { 
+        pushBack(v, i);
+    }
 }
 
 void test_popBack_notEmptyVector(vector *v) { 
@@ -28,10 +24,30 @@ void test_popBack_notEmptyVector(vector *v) {
     }
 }
 
+void test_atVector_notEmptyVector(vector *v) { 
+    printf("%d ", atVector(v, 6));   
+}
+
+void test_atVector_requestToLastElement(vector *v) { 
+    printf("%d ", atVector(v, 5));
+}
+
+void test_back_oneElementInVector(vector *v) { 
+    printf("%d ", back(v));
+}
+
+void test_front_oneElementInVector(vector *v) { 
+    printf("%d ", front(v));
+}
+
 void test(vector *v) { 
     test_pushBack_emptyVector(v);
     test_pushBack_fullVector(v);
     test_popBack_notEmptyVector(v);
+    test_atVector_requestToLastElement(v);
+    test_back_oneElementInVector(v);
+    test_front_oneElementInVector(v);
+    test_atVector_notEmptyVector(v);
 }
 
 int main() { 
--- vector.h
+++ vector.h
@@ -78,3 +78,20 @@ void popBack(vector *v) {
     }
     v->size--;
 }
+
+int* atVector(vector *v, size_t index) { 
+    if(index <= v->size && index >= 0) { 
+        return &v->data[index];
+    } else {
+        printf("IndexError: vector[%d] is not exists", index);
+        exit(1);
+    }
+}
+
+int* back(vector *v) { 
+    return &v->data[v->size];
+}
+
+int* front(vector *v) { 
+    return &v->data[0];
+}
